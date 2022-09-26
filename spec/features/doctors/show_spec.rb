2ec3c85737d3fa99e8d7f4f0396c4c67ed905fc4
Shawn Lee <Shawnl93@hotmail.com>
2022-09-26 09:32:34 -0600

    Set up test for doctors attributes

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe 'doctors show page' do
   end
 
   it "See all of doctors info: name, specialty, uni where they got their doctorate" do
+    visit "/doctors/#{@doctor_1.id}"
 
+    expect(page).to have_content("Name: #{@doctor_1.name}")
+    expect(page).to have_content("Specialty: #{@doctor_1.specialty}")
+    expect(page).to have_content("University: #{@doctor_1.university}")
   end
 end

a8b57dc1d94fbad25f54518fc1cc9173581c2ccb
Shawn Lee <Shawnl93@hotmail.com>
2022-09-26 09:27:24 -0600

    made a show spec for doctors

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -2,6 +2,10 @@ require "rails_helper"
 
 RSpec.describe 'doctors show page' do
   before :each do
+    @doctor_1 = Doctor.create!(name: "Shawn", specialty: "Heart", university: "Turing" )
+  end
+
+  it "See all of doctors info: name, specialty, uni where they got their doctorate" do
 
   end
 end
